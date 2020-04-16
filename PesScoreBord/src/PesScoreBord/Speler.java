d0adecc0a7879d267c8798b1a58398ebe436bdc4
TiboPo <58993536+TiboPo@users.noreply.github.com>
2020-04-16 11:41:56 +0200

    e

---
+++ PesScoreBord/src/PesScoreBord/Speler.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PesScoreBord;
+
+/**
+ *
+ * @author tibop
+ */
+public class Speler {
+    private String naam;
+    private Score score;
+    private int punten;
+
+    public Speler(String naam) {
+        this.naam = naam;
+        this.punten = score.getScore();
+    }
+    
+    public String getNaam(){
+        return naam;
+    }
+    public int getScore(){
+        return punten;
+    }
+   
+    
+}
